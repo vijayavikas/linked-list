43e870aac500be3218ec9dacafd00f3bf933410f
vijayavikas <104316188+vijayavikas@users.noreply.github.com>
2022-10-12 14:21:24 +0530

    Create delete.c

---
+++ delete.c
@@ -0,0 +1,21 @@
+// Delete from beginning
+
+head = head->next;
+
+//Delete from end
+
+struct node* temp = head;
+while(temp->next->next!=NULL){
+  temp = temp->next;
+}
+temp->next = NULL;
+
+//delete from middle
+
+for(int i=2; i< position; i++) {
+  if(temp->next!=NULL) {
+    temp = temp->next;
+  }
+}
+
+temp->next = temp->next->next;
